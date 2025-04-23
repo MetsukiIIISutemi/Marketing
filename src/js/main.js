f709d6fd071f610353aaf460a83bcd33de8cfa30
Metsuki Sutemi <metsuki-sutemi@mail.ru>
2025-04-23 14:40:01 +0300

    added animation and scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,31 @@
 $(function () {
 
+  window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+    document.querySelectorAll('.section').forEach((el, i) => {
+      if (el.offsetTop - document.querySelector('.menu').clientHeight <= scrollDistance) {
+        document.querySelectorAll('.menu a').forEach((el) => {
+          if (el.classList.contains('active')) {
+            el.classList.remove('active');
+          }
+        });
+        document.querySelectorAll('.menu li')[i].querySelector('a').classList.add('active');
+      }
+    });
+  });
+
+
+  $(".menu a").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1200);
+  });
+
+
+
+ 
+
 
   $('.slider__blog-inner').slick({
     dots: true,
@@ -14,6 +40,19 @@ $(function () {
         }
       },
     ]
+  });
+
+
+  AOS.init({
+    disable: function () {
+      let maxWidth = 1200;
+      return window.innerWidth < maxWidth;
+    },
+    startEvent: 'DOMContentLoaded',
+    offset: 100,
+    delay: 100,
+    duration: 1100,
+    once: true
   });
 
   Fancybox.bind("[data-fancybox]", {
